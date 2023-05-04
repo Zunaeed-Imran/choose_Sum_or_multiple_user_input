94b57d3b9216abb64d8edd02e72ede6cca320e50
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-04 11:28:07 +0600

    computes the running total of a list.
    Write a function that computes the running total of a list.

---
+++ sum5.js
@@ -0,0 +1,15 @@
+let num1 = prompt("Enter a Number: ");
+let num2 = prompt("Enter a Number: ");
+let num3 = prompt("Enter a Number: ");
+let num4 = prompt("Enter a Number: ");
+let num5 = prompt("Enter a Number: ");
+let num6 = prompt("Enter a Number: ");
+num1 = Number(num1);
+num2 = Number(num2);
+num3 = Number(num3);
+num4 = Number(num4);
+num5 = Number(num5);
+num6 = Number(num6);
+
+var total = num1 + num2 + num3 + num4 + num5 + num6;
+document.write(total);
